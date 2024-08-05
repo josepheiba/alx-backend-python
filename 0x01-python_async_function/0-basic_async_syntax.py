49e05f837b252ce9d9ed6b274ce4b78251c5bedd
Joseph Eiba <josepheiba@icloud.com>
2024-08-05 22:21:22 +0100

    commit random

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random delay and return it."""
     ranum = random.uniform(0, max_delay)
     await asyncio.sleep(ranum)
