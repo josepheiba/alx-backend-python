fa267cf42dca03eb7f7bcbf348453033761c7d18
Joseph Eiba <josepheiba@icloud.com>
2024-08-08 02:29:33 +0100

    commit random

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Asyncio Generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Wait for a random delay and return it."""
     for _ in range(10):
         await asyncio.sleep(1)
